3f1c088d40bfedd2d29a66f13d2d3855091e5b87
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-11-15 18:45:06 -0400

    💣 CODIFICAÇÃO Criando um componente Initial para armazenar a primeira seção do site

---
+++ src/components/elements/Initial/Initial.js
@@ -0,0 +1,19 @@
+import './Initial.scss';
+
+export default function Initial(){
+
+    return(
+        <aside>
+            <article>
+                <h1>Cool pokemon cards <br/> 
+                which value is <br/> 
+                increasing daily</h1>
+
+                <p>If there is something we love ot world, it's Pokémon. Whether you're <br/>
+                 talking about the original 150.</p>
+            </article>
+            <span className="spanRadius"/>
+            <span className="spanImage"/>
+        </aside>
+    )
+}
