84596d92148cad6b4a8124ddb490ce15b45a0a50
Jordana Ramos <46281839+jojrms@users.noreply.github.com>
2022-11-22 09:45:31 -0400

    💣 CODIFICAÇÃO Ajustando a estilização do Viewer do Pokemon

--- src/components/elements/Viewer/Viewer.js
+++ src/components/elements/Viewer/Viewer.js
@@ -1,13 +1,20 @@
 import './Viewer.scss';
 
-export default function Viewer({id, name, generation, photo_url}){
+export default function Viewer({id, name, typePokemon, photo_url}){
 
     return(
         <span className='spanPokemonViewer'>
             <span className='spanId'>{id}</span>
             <img src={photo_url}/>
             <h1>{name}</h1>
-            <p>{generation}</p>
+
+            <div className='divTypes'>
+                {typePokemon.map( (type) => 
+                    <span className={type.type.name}>{type.type.name}</span>
+                )}    
+            </div>
+            
+
         </span>
     )
 }
